9535dbbf885c486ccebb5adfb10a0f48db37e9cb
F.T. Boie <s2879409@ai178142.ai.rug.nl>
2019-02-28 14:17:29 +0100

    did we fix it?

--- ElfRaus/ElfRaus.swift
+++ ElfRaus/ElfRaus.swift
@@ -12,38 +12,104 @@ import UIKit
 
 class ElfRaus {
     var cards = [Card]()
-    var deck = [Card]()
+    var deck = Array(0...79)
     var cardsPlayer = [Card]()
     var cardsModel = [Card]()
     let totalCards = 80
-    struct legalOption{
-        var color : UIColor
-        var number : Int
-    }
-    var legalOptions = [legalOption]()
+    var cardsInDeck = 80
+    var cardsPlayedGreen = [Int]()
+    var cardsPlayedRed = [Int]()
+    var cardsPlayedBlue = [Int]()
+    var cardsPlayedYellow = [Int]()
+    
+    
+    var legalOptions = [Int:Card]()
     
     func getCardsPlayer() -> [Card]{
         return cardsPlayer
     }
     
+    func drawCard(_ player: String){
+        var cardIndex : Int
+        cardIndex = deck[0]
+        deck.remove(at: 0)
+        if(cardsInDeck > 0){
+            if(player == "Player"){
+                cards[cardIndex].location = "Player"
+                cardsPlayer.append(cards[cardIndex])
+            } else{
+                cards[cardIndex].location = "Model"
+                cardsModel.append(cards[cardIndex])
+            }
+            cardsInDeck -= 1
+        }
+        print(cardIndex)
+    }
+    
+    //add card to array of the color
+    func addPlayedCardToColorArray(in color: UIColor, at index : Int){
+        if(color == UIColor.green){
+            cardsPlayedGreen.append(index)
+        }else if(color == UIColor.blue){
+            cardsPlayedBlue.append(index)
+        }else if(color == UIColor.red){
+            cardsPlayedRed.append(index)
+        }else if(color == UIColor.yellow){
+            cardsPlayedYellow.append(index)
+        }
+    }
+    
+    func chooseCard(at index : Int){
+        print("chooseCard")
+        let indexCardDeck = index //cardsPlayer[index].identifier
+        //let chosenCard = cards[indexCardDeck]
+        if (legalOptions.index(forKey: indexCardDeck) != nil) {
+            print("play card")
+            cards[index].location = "Played"
+            if(cards[index].number > 11), (cards[index].number > 20){
+                legalOptions.updateValue(cards[index+1], forKey: index+1)
+            }else if(cards[index].number > 1){
+                legalOptions.updateValue(cards[index-1], forKey: index-1)
+            }
+            for indexCardPlayer in 0...cardsPlayer.endIndex-1{
+                if(cardsPlayer[indexCardPlayer].identifier == index){
+                    cardsPlayer.remove(at: indexCardPlayer)
+                    break
+                    }
+            }
+            
+        } else{
+            print(indexCardDeck)
+        }
+        
+    }
+    
     init(){
         var colors = [UIColor.yellow, UIColor.green, UIColor.red, UIColor.blue]
         
+        //create all cards
         for color in 0...3 {
             for number in 1...20 {
-                cards.append(Card(color: colors[color],number: number,location: "Deck", identifier : color*number ))
+                cards.append(Card(color: colors[color],number: number,location: "Deck", identifier : color*20+number ))
             }
-            legalOptions.append(legalOption(color: colors[color], number: 11))
+            legalOptions.updateValue(cards[color*20+11], forKey: color*20+11)
         }
-        
+        deck.shuffle()
         //distribute the cards before first turn
-        cards.shuffle()
+        //cards.shuffle()
         let numberOfDistributedCards = 5
-        for card in 0...numberOfDistributedCards {
-            cards[card].location = "Player"
-            cardsPlayer.append(cards[card])
-            cards[card + numberOfDistributedCards].location = "Model"
-            cardsPlayer.append(cards[card + numberOfDistributedCards])
+        for card in 1...numberOfDistributedCards {
+            let cardPlayer = deck[card]
+            deck.remove(at: card)
+            cards[cardPlayer].location = "Player"
+            cardsPlayer.append(cards[cardPlayer])
+            
+            let cardModel = deck[card]
+            deck.remove(at: card)
+            cards[cardModel].location = "Model"
+            cardsModel.append(cards[cardModel])
+            cardsInDeck -= 2
+            print(card)
         }
     }
     
