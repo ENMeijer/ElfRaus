c7002a7b33ad3a64c9fe2f4f080a59b6aa8d5be5
F.T. Boie <s2879409@ai178142.ai.rug.nl>
2019-03-01 10:41:27 +0100

    clean up playingCardView

--- ElfRaus/PlayingCardView.swift
+++ ElfRaus/PlayingCardView.swift
@@ -16,23 +16,19 @@ class PlayingCardView: UIView {
     @IBInspectable
     private var color: UIColor = UIColor.black{ didSet { setNeedsDisplay(); setNeedsLayout() } }
     @IBInspectable
-    private var atLeastOneCard: Bool = false { didSet { setNeedsDisplay(); setNeedsLayout() } }
-    
-    var faceCardScale: CGFloat = SizeRatio.faceCardImageSizeToBoundsSize { didSet { setNeedsDisplay() } }
+    private var atLeastOneCard: Bool = false { didSet { setNeedsDisplay(); setNeedsLayout() } } //card is invisable if no card is given
     
     
     func setCardView(cardNumber:Int){
         number = cardNumber
-        atLeastOneCard = true
+        if cardNumber != 0{
+            atLeastOneCard = true
+        }
     }
     
     func setCardViewColor(cardColor:UIColor){
         color = cardColor
     }
-    
-    
-    
-    
     
     private func centeredAttributedString(_ string: String, fontSize: CGFloat) -> NSAttributedString {
         var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
@@ -42,14 +38,14 @@ class PlayingCardView: UIView {
         return NSAttributedString(string: string, attributes: [.paragraphStyle:paragraphStyle, .font: font, .foregroundColor:color])
     }
     
-    private var cornerString: NSAttributedString {
-        return centeredAttributedString(numberString, fontSize: cornerFontSize)
+    private var cardString: NSAttributedString {
+        return centeredAttributedString(numberString, fontSize: centerFontSize)
     }
     
 
-    private lazy var NumberLabel = createCornerLabel()
+    private lazy var NumberLabel = createCenterLabel()
     
-    private func createCornerLabel() -> UILabel {
+    private func createCenterLabel() -> UILabel {
         let label = UILabel()
         label.numberOfLines = 0
         addSubview(label)
@@ -57,7 +53,7 @@ class PlayingCardView: UIView {
     }
     
     private func configureLabel(_ label: UILabel) {
-        label.attributedText = cornerString
+        label.attributedText = cardString
         label.frame.size = CGSize.zero
         label.sizeToFit()
         label.isHidden = !atLeastOneCard
@@ -72,8 +68,7 @@ class PlayingCardView: UIView {
         super.layoutSubviews()
         
         configureLabel(NumberLabel)
-//        NumberLabel.frame.origin = bounds.origin.offsetBy(dx: cornerOffset, dy: cornerOffset)
-        NumberLabel.center = CGPoint(x: bounds.maxX/2, y: bounds.maxY/2)
+        NumberLabel.center = CGPoint(x: bounds.maxX/2, y: bounds.maxY/2) // center card number lable
     }
     
     
@@ -90,16 +85,6 @@ class PlayingCardView: UIView {
             #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0).setFill()
         }
         roundedRect.fill()
-        
-        if atLeastOneCard {
-            if let faceCardImage = UIImage(named: numberString, in: Bundle(for: self.classForCoder), compatibleWith: traitCollection) {
-                faceCardImage.draw(in: bounds.zoom(by: faceCardScale))
-            }
-        } else {
-            if let cardBackImage = UIImage(named: "cardback", in: Bundle(for: self.classForCoder), compatibleWith: traitCollection) {
-                cardBackImage.draw(in: bounds)
-            }
-        }
     }
     
     
@@ -111,22 +96,16 @@ class PlayingCardView: UIView {
 
 extension PlayingCardView {
     private struct SizeRatio {
-        static let cornerFontSizeToBoundsHeight: CGFloat = 0.55
+        static let centerFontSizeToBoundsHeight: CGFloat = 0.55
         static let cornerRadiusToBoundsHeight: CGFloat = 0.06
-        static let cornerOffsetToCornerRadius: CGFloat = 0.33
-        static let faceCardImageSizeToBoundsSize: CGFloat = 0.75
     }
     
     private var cornerRadius: CGFloat {
         return bounds.size.height * SizeRatio.cornerRadiusToBoundsHeight
     }
     
-    private var cornerOffset: CGFloat {
-        return cornerRadius * SizeRatio.cornerOffsetToCornerRadius
-    }
-    
-    private var cornerFontSize: CGFloat {
-        return bounds.size.height * SizeRatio.cornerFontSizeToBoundsHeight
+    private var centerFontSize: CGFloat {
+        return bounds.size.height * SizeRatio.centerFontSizeToBoundsHeight
     }
     
     private var numberString: String {
