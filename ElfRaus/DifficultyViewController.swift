1888a3083f0fa955468c0be1bb688d88d6b64597
F.T. Boie <s2879409@usabilitylabs-imac-8.local>
2019-03-26 17:21:15 +0100

    additional Controller

---
+++ ElfRaus/DifficultyViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DifficultyViewController.swift
+//  ElfRaus
+//
+//  Created by F.T. Boie on 26/03/2019.
+//  Copyright © 2019 Eline Meijer. All rights reserved.
+//
+
+import UIKit
+
+
+class DifficultyViewController: UIViewControllerAndVariablesPassedAround {
+    func setDifficulty(_ newDifficulty: Int){
+        difficulty = newDifficulty
+    }
+}
